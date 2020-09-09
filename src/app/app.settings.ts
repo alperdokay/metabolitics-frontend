6734e3cdb7967999124901c28f7c2ade4404d9fa
alperdokay <aodokay@gmail.com>
2020-09-09 23:29:18 +0300

    initial commit

---
+++ src/app/app.settings.ts
@@ -0,0 +1,10 @@
+export class AppSettings {
+  public static get API_ENDPOINT(): string {
+    // return 'http://metabolitics.biodb.sehir.edu.tr/api';
+    return 'http://127.0.0.1:5000';
+  }
+
+  public static get NOTIFICATION_OPTIONS() {
+    return { timeOut: 10000 };
+  }
+}
