3707da6cc572bd455e5436e5eeddc9127b2865b5
paltaj <tajothman@std.sehir.edu.tr>
2020-09-10 10:34:30 +0300

    settings url changed

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,7 +1,7 @@
 export class AppSettings {
   public static get API_ENDPOINT(): string {
-    // return 'http://metabolitics.biodb.sehir.edu.tr/api';
-    return 'http://127.0.0.1:5000';
+    return 'http://metabolitics.biodb.sehir.edu.tr/api';
+    // return 'http://127.0.0.1:5000';
   }
 
   public static get NOTIFICATION_OPTIONS() {
