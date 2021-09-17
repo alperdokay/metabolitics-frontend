654e5e7f37b091aff5e058753c9efa1dd1ac9fd7
Taj <tajothman@std.sehir.edu.tr>
2021-09-17 03:17:17 +0300

    Update app.settings.ts

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,7 +1,7 @@
 export class AppSettings {
   public static get API_ENDPOINT(): string {
     //return 'http://metabolitics.biodb.sehir.edu.tr/api';
-    return 'http://164.90.180.104:5000/api';
+    return 'http://164.90.180.104/api';
      // return 'http://127.0.0.1:5000';
   }
 
